3bfb9b2568ab8a954a8bb2e29e88046bab5ee93e
Gustavo Ayala <gayalaand@gmail.com>
2021-11-10 15:55:53 -0800

    added a collections/favorite

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ const $searchForm = document.querySelector("#searchForm");
 const $foundCont = document.querySelector(".foundCont");
 const $saveBtn = document.querySelector(".saveBtn");
 const $commentForm = document.querySelector(".commentForm");
+const $collectionsCont = document.querySelector(".collectionsCont");
+const $homePage = document.querySelector(".collections");
 
 /*                       HANDLE SEARCH                                      */
 const handleSearch = (event) => {
   event.preventDefault();
   const inputValue = $searchForm.searchInput.value;
-  // $foundCont.remove();
   searchAPI(inputValue);
   $searchForm.reset();
 };
@@ -27,66 +28,68 @@ const searchAPI = (search) => {
     const response = xhr.response;
     console.log(xhr.status);
     console.log(response);
-    for (let i = 0; i < response.results.length; i++) {
-      const dataView = data.nextEntryId++;
-
-      const mainDiv = document.createElement("div");
-      const imgDiv = document.createElement("div");
-      const img = document.createElement("img");
-      const infoDiv = document.createElement("div");
-      const title = document.createElement("h2");
-      const paragraph = document.createElement("p");
-      const saveBtn = document.createElement("button");
-      const commentForm = document.createElement("form");
-      const textarea = document.createElement("textarea");
-      const extraDiv = document.createElement("div");
-      const extraOne = document.createElement("div");
-      const deleteAnchor = document.createElement("button");
-      const saveColDiv = document.createElement("div");
-      const saveAnchor = document.createElement("button");
-
-      mainDiv.className = "lowrow fullCol thirdCol fifthCol resultsCont";
-      mainDiv.setAttribute("data-view", dataView);
-      imgDiv.className = "halfCol resultsCol";
-      infoDiv.className = "halfCol infoSect resultsCol";
-      img.setAttribute("src", response.results[i].image);
-      saveBtn.setAttribute("class", "saveBtn");
-      commentForm.className = "commentForm hidden";
-      textarea.setAttribute("name", "comments");
-      textarea.setAttribute("class", "comments");
-      textarea.setAttribute("id", "comments");
-      textarea.setAttribute("placeholder", "Add comments");
-      extraDiv.setAttribute("class", "row");
-      extraOne.setAttribute("class", "halfCol");
-      deleteAnchor.setAttribute("class", "deleteComment");
-      saveColDiv.setAttribute("class", "halfCol");
-      saveAnchor.setAttribute("class", "addComment");
-      saveBtn.setAttribute("data-view", dataView);
-
-      title.textContent = response.results[i].title;
-      paragraph.textContent = response.results[i].description;
-      saveBtn.textContent = "Save";
-      deleteAnchor.textContent = "Delete Comment";
-      saveAnchor.textContent = "Add Comment";
-
-      mainDiv.appendChild(imgDiv);
-      imgDiv.appendChild(img);
-      mainDiv.appendChild(infoDiv);
-      infoDiv.appendChild(title);
-      infoDiv.appendChild(paragraph);
-      infoDiv.appendChild(saveBtn);
-      infoDiv.appendChild(commentForm);
-      commentForm.appendChild(textarea);
-      extraOne.appendChild(deleteAnchor);
-      extraDiv.appendChild(extraOne);
-      commentForm.appendChild(extraDiv);
-      saveColDiv.appendChild(saveAnchor);
-      extraDiv.appendChild(saveColDiv);
-
-      $foundCont.appendChild(mainDiv);
-    }
+    for (let i = 0; i < response.results.length; i++)
+      createFoundElement(response.results[i], $foundCont);
   });
   xhr.send();
+};
+const createFoundElement = (results, container) => {
+  const dataView = data.nextEntryId++;
+
+  const mainDiv = document.createElement("div");
+  const imgDiv = document.createElement("div");
+  const img = document.createElement("img");
+  const infoDiv = document.createElement("div");
+  const title = document.createElement("h2");
+  const paragraph = document.createElement("p");
+  const saveBtn = document.createElement("button");
+  const commentForm = document.createElement("form");
+  const textarea = document.createElement("textarea");
+  const extraDiv = document.createElement("div");
+  const extraOne = document.createElement("div");
+  const deleteAnchor = document.createElement("button");
+  const saveColDiv = document.createElement("div");
+  const saveAnchor = document.createElement("button");
+
+  mainDiv.className = "lowrow fullCol thirdCol fifthCol resultsCont";
+  mainDiv.setAttribute("data-view", dataView);
+  imgDiv.className = "halfCol resultsCol";
+  infoDiv.className = "halfCol infoSect resultsCol";
+  img.setAttribute("src", results.image);
+  saveBtn.setAttribute("class", "saveBtn");
+  commentForm.className = "commentForm hidden";
+  textarea.setAttribute("name", "comments");
+  textarea.setAttribute("class", "comments");
+  textarea.setAttribute("id", "comments");
+  textarea.setAttribute("placeholder", "Add comments");
+  extraDiv.setAttribute("class", "row");
+  extraOne.setAttribute("class", "halfCol");
+  deleteAnchor.setAttribute("class", "deleteComment");
+  saveColDiv.setAttribute("class", "halfCol");
+  saveAnchor.setAttribute("class", "addComment");
+  saveBtn.setAttribute("data-view", dataView);
+
+  title.textContent = results.title;
+  paragraph.textContent = results.description;
+  saveBtn.textContent = "Save";
+  deleteAnchor.textContent = "Delete Comment";
+  saveAnchor.textContent = "Add Comment";
+
+  mainDiv.appendChild(imgDiv);
+  imgDiv.appendChild(img);
+  mainDiv.appendChild(infoDiv);
+  infoDiv.appendChild(title);
+  infoDiv.appendChild(paragraph);
+  infoDiv.appendChild(saveBtn);
+  infoDiv.appendChild(commentForm);
+  commentForm.appendChild(textarea);
+  extraOne.appendChild(deleteAnchor);
+  extraDiv.appendChild(extraOne);
+  commentForm.appendChild(extraDiv);
+  saveColDiv.appendChild(saveAnchor);
+  extraDiv.appendChild(saveColDiv);
+
+  container.appendChild(mainDiv);
 };
 
 /*                       SAVING COMMENTS                                      */
@@ -114,6 +117,20 @@ const addComment = (event) => {
       commentReplaced.innerText = `"${value}"`;
       formParent.prepend(commentReplaced);
       textareaNear.classList.add("hidden");
+      const title = target.parentElement.children[0].innerText;
+      const description = target.parentElement.children[1].innerText;
+      const imgLink =
+        target.parentElement.parentElement.children[0].children[0].getAttribute(
+          "src"
+        );
+      const entryObj = {
+        title: title,
+        description: description,
+        image: imgLink,
+        comment: commentReplaced.innerText,
+      };
+      data.results.push(entryObj);
+      console.log(data);
     } else if (submitter.getAttribute("class") === "deleteComment") {
       formParent.classList.add("hidden");
       formParent.reset();
@@ -123,8 +140,19 @@ const addComment = (event) => {
     }
   }
 };
+/*                       SAVING TO COLLECTIONS                                      */
+const saveCollections = (event) => {
+  const target = event.target;
+  $foundCont.classList.add("hidden");
+  $collectionsCont.classList.remove("hidden");
+  for (let i = 0; i < data.results.length; i++) {
+    createFoundElement(data.results[i], $collectionsCont);
+    console.log(data);
+  }
+};
 
 /*                       EVENT LISTENERS                                       */
 window.addEventListener("click", saveClick);
+$homePage.addEventListener("click", saveCollections);
 $searchForm.addEventListener("submit", handleSearch);
 window.addEventListener("submit", addComment);
